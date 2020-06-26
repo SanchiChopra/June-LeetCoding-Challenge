0ad6cb03b072023279e607c7b665f683156669a1
Sanchi Chopra <35348387+SanchiChopra@users.noreply.github.com>
2020-06-26 14:07:30 +0530

    Sum Root to Leaf Numbers

---
+++ Day26.cpp
@@ -0,0 +1,72 @@
+/* QUESTION: Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+
+An example is the root-to-leaf path 1->2->3 which represents the number 123.
+
+Find the total sum of all root-to-leaf numbers.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Input: [1,2,3]
+    1
+   / \
+  2   3
+Output: 25
+Explanation:
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+Therefore, sum = 12 + 13 = 25.
+Example 2:
+
+Input: [4,9,0,5,1]
+    4
+   / \
+  9   0
+ / \
+5   1
+Output: 1026
+Explanation:
+The root-to-leaf path 4->9->5 represents the number 495.
+The root-to-leaf path 4->9->1 represents the number 491.
+The root-to-leaf path 4->0 represents the number 40.
+Therefore, sum = 495 + 491 + 40 = 1026.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        int ans=0;
+        sumNumbers(root,0,ans);
+        return ans;
+    }
+    
+    void sumNumbers(TreeNode* root, int data, int &ans)
+    {
+        if(root==NULL)return;
+        data = data*10+root->val;
+        if(!root->left && !root->right)
+        {
+            ans+=data;
+        }
+        if(root->left)
+        {
+            sumNumbers(root->left,data,ans);
+        }
+        if(root->right)
+        {
+            sumNumbers(root->right,data,ans);
+        }
+    }
+};
